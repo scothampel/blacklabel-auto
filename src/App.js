cf57df37d72213e4db5ba5e5bef1babde76818bf
Scot Hampel <scot@scothampel.com>
2021-04-17 22:48:42 -0500

    Added hero placeholder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './css/App.css';
 import Nav from './Nav';
 import Footer from './Footer';
+import Hero from './Hero';
 
 function App() {
   return (
     <div className='App' id='app'>
       <Nav />
-      <div style={{'height': '3000px'}}></div>
+      <Hero />
       <Footer />
     </div>
   );
---
+++ src/Hero.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './css/Hero.css';
+
+export default function Hero() {
+    return (
+        <div className='hero font-primary'>
+            Some placeholder text
+        </div>
+    )
+}
