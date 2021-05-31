d78132727ba75b1f084eb9a197a1e659829b15c0
Scot Hampel <scot@scothampel.com>
2021-05-31 00:04:14 -0500

    Adjust padding/margin

--- src/Hero.js
+++ src/Hero.js
@@ -3,7 +3,7 @@ import './css/Hero.css';
 
 export default function Hero() {
     return (
-        <div className='container hero font-primary'>
+        <div className='container hero font-primary mt-2'>
             Some placeholder text
         </div>
     )
