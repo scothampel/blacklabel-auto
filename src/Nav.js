0c561a10359229545ab5c8e9645be0aa165c846a
Scot Hampel <scot@scothampel.com>
2021-05-31 00:17:58 -0500

    Format

--- src/Nav.js
+++ src/Nav.js
@@ -4,36 +4,36 @@ import logo from './logo.svg';
 
 export default function Nav() {
     return (
-            <nav className='navbar sticky-top navbar-expand-lg navbar-light pb-0'>
-                <div className='container border-bottom border-dark pb-2'>
-                    <div className='row col m-0'>
+        <nav className='navbar sticky-top navbar-expand-lg navbar-light pb-0'>
+            <div className='container border-bottom border-dark pb-2'>
+                <div className='row col m-0'>
                     <a className='navbar-brand d-flex align-items-center col-auto m-0' href='#app'>
-                        <img src={logo} width='100px' alt='Black Label Auto Detailing'/>
+                        <img src={logo} width='100px' alt='Black Label Auto Detailing' />
                         <h2 className='d-none d-md-inline d-lg-none ms-3 mb-0 font-primary'>BLACK LABEL AUTO DETAILING</h2>
-                        
+
                     </a>
-                    <h4 className='d-md-none col-6 d-flex align-items-center font-primary text-wrap'>BLACK LABEL <br/>AUTO DETAILING</h4>
+                    <h4 className='d-md-none col-6 d-flex align-items-center font-primary text-wrap'>BLACK LABEL <br />AUTO DETAILING</h4>
                     <button className='navbar-toggler col-auto border-0 p-0 ms-auto' type='button' data-bs-toggle='collapse' data-bs-target='#navContent'>
                         <i className="bi bi-list"></i>
                     </button>
-                    </div>
-                    <div className='collapse navbar-collapse' id='navContent'>
-                        <ul className='navbar-nav ms-lg-auto mb-lg-0 row'>
-                            <li className='nav-item col-auto'>
-                                <a className='nav-link link-black' href='#services'>SERVICES</a>
-                            </li>
-                            <li className='nav-item col-auto'>
-                                <a className='nav-link link-black' href='#portfolio'>PORTFOLIO</a>
-                            </li>
-                            <li className='nav-item col-auto'>
-                                <a className='nav-link link-black' href='#about'>ABOUT</a>
-                            </li>
-                            <li className='nav-item col-auto'>
-                                <a className='nav-link link-black' href='#contact'>CONTACT</a>
-                            </li>
-                        </ul>
-                    </div>
                 </div>
-            </nav>
+                <div className='collapse navbar-collapse' id='navContent'>
+                    <ul className='navbar-nav ms-lg-auto mb-lg-0 row'>
+                        <li className='nav-item col-auto'>
+                            <a className='nav-link link-black' href='#services'>SERVICES</a>
+                        </li>
+                        <li className='nav-item col-auto'>
+                            <a className='nav-link link-black' href='#portfolio'>PORTFOLIO</a>
+                        </li>
+                        <li className='nav-item col-auto'>
+                            <a className='nav-link link-black' href='#about'>ABOUT</a>
+                        </li>
+                        <li className='nav-item col-auto'>
+                            <a className='nav-link link-black' href='#contact'>CONTACT</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     )
 }
