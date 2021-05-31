baaf2e5fc2ade2f5e278fc0a8bf75ba91d532611
Scot Hampel <scot@scothampel.com>
2021-05-30 23:01:05 -0500

    Include services

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './css/App.css';
 import Nav from './Nav';
 import Footer from './Footer';
 import Hero from './Hero';
+import Services from './Services';
 
 function App() {
   return (
     <div className='App' id='app'>
       <Nav />
       <Hero />
+      <Services />
       <Footer />
     </div>
   );
---
+++ src/Services.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import './css/Services.css';
+
+export default function Services() {
+    return (
+        <div className='container' id='services'>
+            <div className='row row-cols-1 row-cols-md-3 g-4 mt-0 mb-4'>
+                <div className='col'>
+                    <div className='card border-0'>
+                        <div className='card-body'>
+                            <h4 className='card-title font-primary'>PLACEHOLDER</h4>
+                            <hr />
+                            <p className='card-text'>jkwfjwkwfjwfkjawhfkwajfkajfajkwfkj</p>
+                        </div>
+                    </div>
+                </div>
+                <div className='col'>
+                    <div className='card border-0'>
+                        <div className='card-body'>
+                            <h4 className='card-title font-primary'>PLACEHOLDER</h4>
+                            <hr />
+                            <p className='card-text'>jkwfjwkwfjwfkjawhfkwajfkajfajkwfkj</p>
+                        </div>
+                    </div>
+                </div>
+                <div className='col'>
+                    <div className='card border-0'>
+                        <div className='card-body'>
+                            <h4 className='card-title font-primary'>PLACEHOLDER</h4>
+                            <hr />
+                            <p className='card-text'>jkwfjwkwfjwfkjawhfkwajfkajfajkwfkj</p>
+                        </div>
+                    </div>
+                </div>
+                <div className='col'>
+                    <div className='card border-0'>
+                        <div className='card-body'>
+                            <h4 className='card-title font-primary'>PLACEHOLDER</h4>
+                            <hr />
+                            <p className='card-text'>jkwfjwkwfjwfkjawhfkwajfkajfajkwfkj</p>
+                        </div>
+                    </div>
+                </div>
+                <div className='col'>
+                    <div className='card border-0'>
+                        <div className='card-body'>
+                            <h4 className='card-title font-primary'>PLACEHOLDER</h4>
+                            <hr />
+                            <p className='card-text'>jkwfjwkwfjwfkjawhfkwajfkajfajkwfkj</p>
+                        </div>
+                    </div>
+                </div>
+                <div className='col'>
+                    <div className='card border-0'>
+                        <div className='card-body'>
+                            <h4 className='card-title font-primary'>PLACEHOLDER</h4>
+                            <hr />
+                            <p className='card-text'>jkwfjwkwfjwfkjawhfkwajfkajfajkwfkj</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
