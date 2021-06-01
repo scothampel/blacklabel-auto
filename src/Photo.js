3da7fb902a9179eb0b48e7acbcb5a54a7292ee3f
Scot Hampel <scot@scothampel.com>
2021-05-31 22:22:51 -0500

    Add alt

--- src/Photo.js
+++ src/Photo.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Photo({photoUrl}) {
     return (
         <div className='col'>
-            <img src={photoUrl} className='img-fluid img-thumbnail' />
+            <img src={photoUrl} className='img-fluid img-thumbnail' alt='Portfolio entry' />
         </div>
     )
 }
