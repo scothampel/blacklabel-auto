7be3e84d308fa60bc43195dd92d1b5f98651208d
Scot Hampel <scot@scothampel.com>
2021-06-01 19:44:10 -0500

    Add contact

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import Nav from './Nav';
 import Footer from './Footer';
 import Hero from './Hero';
 import Services from './Services';
-import Portfolio from './Portfolio'
+import Portfolio from './Portfolio';
+import Contact from './Contact';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Hero />
       <Services />
       <Portfolio />
+      <Contact />
       <Footer />
     </div>
   );
