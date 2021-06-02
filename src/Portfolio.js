fc615513257eb61e4c5405fe08e0249e825dfa91
Scot Hampel <scot@scothampel.com>
2021-06-01 20:05:42 -0500

    Conditional rendering and auto adjustment

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -4,19 +4,23 @@ import './css/Portfolio.css';
 import logo from './logo.svg';
 
 export default function Portfolio() {
+    // Change on photo addition until CMS integration
+    let photoCount = 7;
     return (
         <div className='container' id='portfolio'>
             <div className='row row-cols-1 row-cols-md-3 g-4 mt-0 mb-4'>
-                <Photo photoUrl='/photos/1.jpg'/>
-                <Photo photoUrl='/photos/2.jpg'/>
-                <Photo photoUrl='/photos/3.jpg'/>
-                <Photo photoUrl='/photos/4.jpg'/>
-                <Photo photoUrl='/photos/5.jpg'/>
-                <Photo photoUrl='/photos/6.jpg'/>
-                <Photo photoUrl='/photos/7.jpg'/>
-                <div className='col-md-8 d-none d-md-block'>
-                    <img src={logo} id='logo-ph' alt='Logo placeholder' />
-                </div>
+                <Photo photoUrl='/photos/1.jpg' />
+                <Photo photoUrl='/photos/2.jpg' />
+                <Photo photoUrl='/photos/3.jpg' />
+                <Photo photoUrl='/photos/4.jpg' />
+                <Photo photoUrl='/photos/5.jpg' />
+                <Photo photoUrl='/photos/6.jpg' />
+                <Photo photoUrl='/photos/7.jpg' />
+                { 9 % photoCount !== 0 &&
+                    <div className={`col-md-${9 % photoCount * 4} d-none d-md-block`}>
+                        <img src={logo} id='logo-ph' alt='Logo placeholder' />
+                    </div>
+                }
             </div>
         </div>
     )
