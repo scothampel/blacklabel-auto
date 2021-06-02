d13e628f1e4e350430c92d76ac6aa4d88a613b06
Scot Hampel <scot@scothampel.com>
2021-06-02 16:10:08 -0500

    Fix scroll issue

--- src/Nav.js
+++ src/Nav.js
@@ -1,49 +1,64 @@
-import React, {useEffect} from 'react';
-import './css/Nav.css';
-import logo from './logo.svg';
-import $ from 'jquery';
+import React, { useEffect } from "react";
+import "./css/Nav.css";
+import logo from "./logo.svg";
+import $ from "jquery";
 
 export default function Nav() {
-    useEffect(() => {
-        $('.nav-link').on('click', () => {
-            if($('.collapse').hasClass('show')){
-                $('.navbar-toggler').click();
-            }
-        });
-        $('.navbar-brand').on('click', () =>{
-            if($('.collapse').hasClass('show')){
-                $('.navbar-toggler').click();
-            }
-        });
-    }, [])
+  useEffect(() => {
+    $(".nav-link").on("click", () => {
+      $(".collapse").removeClass("show");
+    });
+    $(".navbar-brand").on("click", () => {
+      $(".collapse").removeClass("show");
+    });
+  }, []);
 
-    return (
-        <nav className='navbar sticky-top navbar-expand-lg navbar-light pb-0'>
-            <div className='container border-bottom border-dark pb-2'>
-                <div className='row col m-0'>
-                    <a className='navbar-brand d-flex align-items-center col-auto m-0' href='#app'>
-                        <img src={logo} width='100px' alt='Black Label Auto Detailing' />
-                        <h2 className='d-none d-md-inline d-lg-none ms-3 mb-0 font-primary'>BLACK LABEL AUTO DETAILING</h2>
-                    </a>
-                    <h4 className='d-md-none col-6 d-flex align-items-center font-primary text-wrap'>BLACK LABEL <br />AUTO DETAILING</h4>
-                    <button className='navbar-toggler col-auto border-0 p-0 ms-auto' type='button' data-bs-toggle='collapse' data-bs-target='#navContent'>
-                        <i className="bi bi-list"></i>
-                    </button>
-                </div>
-                <div className='collapse navbar-collapse' id='navContent'>
-                    <ul className='navbar-nav ms-lg-auto mb-lg-0 row'>
-                        <li className='nav-item col-auto'>
-                            <a className='nav-link link-black' href='#services'>SERVICES</a>
-                        </li>
-                        <li className='nav-item col-auto'>
-                            <a className='nav-link link-black' href='#portfolio'>PORTFOLIO</a>
-                        </li>
-                        <li className='nav-item col-auto'>
-                            <a className='nav-link link-black' href='#contact'>CONTACT</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    )
+  return (
+    <nav className="navbar sticky-top navbar-expand-lg navbar-light pb-0">
+      <div className="container border-bottom border-dark pb-2">
+        <div className="row col m-0">
+          <a
+            className="navbar-brand d-flex align-items-center col-auto m-0"
+            href="#app"
+          >
+            <img src={logo} width="100px" alt="Black Label Auto Detailing" />
+            <h2 className="d-none d-md-inline d-lg-none ms-3 mb-0 font-primary">
+              BLACK LABEL AUTO DETAILING
+            </h2>
+          </a>
+          <h4 className="d-md-none col-6 d-flex align-items-center font-primary text-wrap">
+            BLACK LABEL <br />
+            AUTO DETAILING
+          </h4>
+          <button
+            className="navbar-toggler col-auto border-0 p-0 ms-auto"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navContent"
+          >
+            <i className="bi bi-list"></i>
+          </button>
+        </div>
+        <div className="collapse navbar-collapse" id="navContent">
+          <ul className="navbar-nav ms-lg-auto mb-lg-0 row">
+            <li className="nav-item col-auto">
+              <a className="nav-link link-black" href="#services">
+                SERVICES
+              </a>
+            </li>
+            <li className="nav-item col-auto">
+              <a className="nav-link link-black" href="#portfolio">
+                PORTFOLIO
+              </a>
+            </li>
+            <li className="nav-item col-auto">
+              <a className="nav-link link-black" href="#contact">
+                CONTACT
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
