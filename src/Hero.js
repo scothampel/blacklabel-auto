cef08657ee03adee65bec01a0b2c5af20b93b9ee
Scot Hampel <scot@scothampel.com>
2021-06-01 21:19:12 -0500

    Hero text center

--- src/Hero.js
+++ src/Hero.js
@@ -5,7 +5,7 @@ export default function Hero() {
     return (
         <div className='container' id='hero'>
             <div className='hero font-primary mt-2 mb-4'>
-                <h1>CAR CARE PROFESSIONALS</h1>
+                <h1 className='text-center'>CAR CARE PROFESSIONALS</h1>
             </div>
         </div>
     )
