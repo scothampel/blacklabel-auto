0a97c094dd110256a056f7cb9b59605ee63aa41f
Scot Hampel <scot@scothampel.com>
2021-06-10 10:11:00 -0500

    Contact md size adjust

--- src/Contact.js
+++ src/Contact.js
@@ -4,7 +4,7 @@ import './css/Contact.css';
 export default function Contact() {
     return (
         <div className='container' id='contact'>
-            <div className='text-center py-4 mb-4 w-md-50'>
+            <div className='text-center py-4 mb-4'>
                 <h1 className='font-primary'><b>Kevin Reissenweber</b></h1>
                 <h3>630.388.9670 (call or text)</h3>
                 <h3>St. Charles, IL</h3>
