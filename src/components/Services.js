6450c6f9234f77c1e5099aeb595f7aeb59e36005
Scot Hampel <scot@scothampel.com>
2021-07-29 16:50:50 -0500

    Add aditional services

--- src/components/Services.js
+++ src/components/Services.js
@@ -7,7 +7,8 @@ export default function Services() {
                 <div className='col'>
                     <div className='card card-black border-0'>
                         <div className='card-body'>
-                            <h4 className='card-title font-primary'>Full Interior / Exterior Detail<br/>$225.00</h4>
+                            <h4 className='card-title font-primary'>Full Interior / Exterior Detail</h4>
+                            <h5 className='font-primary text-muted'>$225</h5>
                             <hr />
                             <ul>
                                 <li>Hand washed exterior</li>
@@ -28,7 +29,8 @@ export default function Services() {
                 <div className='col'>
                     <div className='card card-black border-0'>
                         <div className='card-body'>
-                            <h4 className='card-title font-primary'>Full Interior Detail<br/>$125.00</h4>
+                            <h4 className='card-title font-primary'>Full Interior Detail</h4>
+                            <h5 className='font-primary text-muted'>$125</h5>
                             <hr />
                             <ul>
                                 <li>Thorough vacuum of the interior including the trunk</li>
@@ -46,7 +48,8 @@ export default function Services() {
                 <div className='col'>
                     <div className='card card-black border-0'>
                         <div className='card-body'>
-                            <h4 className='card-title font-primary'>Full Exterior Ceramic Detail<br/>$150.00</h4>
+                            <h4 className='card-title font-primary'>Full Exterior Ceramic Detail</h4>
+                            <h5 className='font-primary text-muted'>$150</h5>
                             <hr />
                             <ul>
                                 <li>Hand washed exterior</li>
@@ -56,6 +59,71 @@ export default function Services() {
                                 <li>Clean all exterior glass</li>
                                 <li className='li-check'> <i>8 months of protection on your paint</i></li>
                                 <li className='li-check'> <i>Coating prevents dirt, mud, salt, oil, tar and brake dust from sticking to your vehicle and damaging it over time</i></li>
+                            </ul>
+                            <p className='card-text'><i>Pick-up and delivery of vehicle available</i></p>
+                        </div>
+                    </div>
+                </div>
+                <div className='col'>
+                    <div className='card card-black border-0'>
+                        <div className='card-body'>
+                            <h4 className='card-title font-primary'>Gtechniq EXOv4 Ceramic Coating</h4>
+                            <h5 className='font-primary text-muted'>Cars: $250<br />Midsized: $300 <br /> Oversized: $350</h5>
+                            <hr />
+                            <ul>
+                                <li>Up to 2 years of protection</li>
+                                <li>Delivers a hard film that makes water-spots easy to remove</li>
+                                <li>Offers great resistance to all wash chemicals and can help withstand micro-marring</li>
+                                <li>Produces a high-gloss, warm-looking, extreme hydrophobic finish</li>
+                            </ul>
+                            <p className='card-text'><i>Pick-up and delivery of vehicle available</i></p>
+                        </div>
+                    </div>
+                </div>
+                <div className='col'>
+                    <div className='card card-black border-0'>
+                        <div className='card-body'>
+                            <h4 className='card-title font-primary'>Gtechniq Crystal Serum Light Ceramic Coating</h4>
+                            <h5 className='font-primary text-muted'>Cars: $600<br />Midsized: $700 <br /> Oversized: $800</h5>
+                            <hr />
+                            <ul>
+                                <li className='li-check'>Up to 3 years of protection</li>
+                                <li>Offers 80% of professional Serum Ultra’s performance</li>
+                                <li>Can be machine polished from your vehicle's paintwork if removal is needed</li>
+                                <li>Adds gloss, resists swirls, and repels contaminants and dirt</li>
+                                <li>The pinnacle in paint protection</li>
+                            </ul>
+                            <p className='card-text'><i>Pick-up and delivery of vehicle available</i></p>
+                        </div>
+                    </div>
+                </div>
+                <div className='col'>
+                    <div className='card card-black border-0'>
+                        <div className='card-body'>
+                            <h4 className='card-title font-primary'>Light Paint Correction (6+&nbsp;Hours)</h4>
+                            <h5 className='font-primary text-muted'>Cars: $450<br />Midsized: $530 <br /> Oversized: $610</h5>
+                            <hr />
+                            <ul>
+                                <li>Hand washed and fully decontaminated of oils and waxes</li>
+                                <li>Clay Bar treatment</li>
+                                <li>1 stage paint correction to remove minor swirls and paint blemishes</li>
+                                <li>Sealed with a high quality carnauba wax or sealant</li>
+                            </ul>
+                            <p className='card-text'><i>Pick-up and delivery of vehicle available</i></p>
+                        </div>
+                    </div>
+                </div>
+                <div className='col'>
+                    <div className='card card-black border-0'>
+                        <div className='card-body'>
+                            <h4 className='card-title font-primary'>Multi-Stage Paint Correction (1-3&nbsp;Days)</h4>
+                            <h5 className='font-primary text-muted'>Call for a quote. Requires an in-person paint inspection</h5>
+                            <hr />
+                            <ul>
+                                <li>Hand washed and fully decontaminated of oils and waxes</li>
+                                <li>Clay Bar treatment</li>
+                                <li>2-4 stages of paint correction to get the paint as defect free as possible</li>
+                                <li>Sealed with whatever coating or sealant you prefer. We recommend a ceramic coating at this point</li>
                             </ul>
                             <p className='card-text'><i>Pick-up and delivery of vehicle available</i></p>
                         </div>
